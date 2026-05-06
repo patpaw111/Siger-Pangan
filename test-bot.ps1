91619cebc1975540f8f39eb81968bab90ca9338c
patpaw111 <mfaqih3562@gmail.com>
2026-05-07 05:29:17 +0700

    docs: add final report PDF and update gitignore

--- test-bot.ps1
+++ test-bot.ps1
@@ -6,7 +6,7 @@ $body = @{ text = $Pertanyaan } | ConvertTo-Json
 $response = Invoke-RestMethod -Method Post -Uri "http://localhost:3000/api/v1/chatbot/chat" -ContentType "application/json" -Body $body
 
 Write-Host "=============================" -ForegroundColor Cyan
-Write-Host "🤖 Pertanyaan : $Pertanyaan" -ForegroundColor Yellow
+Write-Host "Pertanyaan : $Pertanyaan" -ForegroundColor Yellow
 Write-Host "=============================" -ForegroundColor Cyan
 
 Write-Host "Jawaban Bot   :`n" -ForegroundColor Green
